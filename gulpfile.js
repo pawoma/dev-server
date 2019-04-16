c0f5a6c4c0f667fb4d7880eea5a164b3b04a7fae
pcli <294805740@qq.com>
2019-04-16 19:28:20 +0800

    add less to css plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@ const serve = require('gulp-serve')
 const livereload = require('gulp-livereload')
 const gulp = require('gulp')
 const path = require('path')
+
 const inject = require('connect-livereload')()
+
+const less = require('gulp-less');
+const LessAutoprefix = require('less-plugin-autoprefix');
+const autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+
+// const md5 = require("gulp-md5-plus");
 
 const rootPath = path.resolve(__dirname, './www')
 
@@ -14,20 +21,75 @@ gulp.task('watch', function () {
 
     let watcher = gulp.watch(rootPath)
     watcher.on('change', function (e) {
+        console.log(e)
+
+        if (path.extname(e) === '.less') {
+            let dir = path.resolve(e, '..')
+            gulp.src(e)
+                .pipe(less({
+                    plugins: [autoprefix]
+                }))
+                .pipe(gulp.dest(dir))
+                .on('end',function(){
+                    let fileName = path.basename(e,'.less')
+                    let fileSrc =  path.join(dir,`${fileName}.css`)
+                    livereload.changed(fileSrc)
+                })  
+
+        }
         livereload.changed(e)
     })
 })
 
 
+// gulp.task('less', function (e) {
+//     let dir = path.resolve(e, '..')
+//     return gulp.src(e)
+//         .pipe(less({
+//             plugins: [autoprefix]
+//         }))
+//         .pipe(gulp.dest(dir));
+// });
+
 gulp.task('serve', serve({
     root: [rootPath],
-    hostname: 'localhost',
-    port: 3001,
+    hostname: '0.0.0.0',
+    port: 8000,
     // inject livereload script ot html
     middleware: inject
 }))
 
 gulp.task('default', gulp.parallel('serve', 'watch'))
+
+
+// const gulp = require('gulp');
+// const webserver = require('gulp-webserver');
+
+// gulp.task('webserver', function () {
+//     gulp.src('./www')
+//         .pipe(webserver({
+//             host: '192.168.1.185',
+//             port: 8000,
+//             livereload: true,
+//             // open: '/',
+//             directoryListing: {
+//                 enable: true,
+//                 path: './www'
+//             },
+//             proxies: [
+//                 {
+//                     source: '/RongoList/', target: 'http://wx.rongo.net.cn/RongoList/'
+//                 },
+//                 {
+//                     source: '/JSSDK/', target: 'http://wx.rongo.net.cn/JSSDK/'
+//                 }
+//             ]
+//         }))
+// });
+// gulp.task('default', gulp.parallel('webserver'));
+
+
+
 
 
 
